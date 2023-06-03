2105055a06b9f827aec3705cab8a44ff34841923
darshan15062002 <darshanjain15062002@gmail.com>
2023-06-03 21:14:36 +0530

    sdvsv

--- server/data/database.js
+++ server/data/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        const { connection } = await mongoose.connect(process.env.MOGO_URI)
+        const { connection } = await mongoose.connect('mongodb+srv://1032200336:$$dar$$123@cluster0.xwqwfn6.mongodb.net/college')
         console.log(`server connected to database ${connection.host}`);
     } catch (error) {
         console.log('some Error occured', error);
