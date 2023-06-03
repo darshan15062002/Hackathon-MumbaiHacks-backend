7d0786ac6b38cea14c600f7856980d67f8b8d89a
darshan15062002 <darshanjain15062002@gmail.com>
2023-06-03 21:51:27 +0530

    sdvsv

--- server/data/database.js
+++ server/data/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        const { connection } = await mongoose.connect('mongodb+srv://1032200336:$$dar$$123@cluster0.xwqwfn6.mongodb.net/college')
+        const { connection } = await mongoose.connect('mongodb+srv://1032200336:$$dar$$123@cluster0.xwqwfn6.mongodb.net/college', { useNewUrlParser: true, useUnifideTopology: true, useCreateIndex: true })
         console.log(`server connected to database ${connection.host}`);
     } catch (error) {
         console.log('some Error occured', error);
